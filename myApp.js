2cfff230f3e4e45f4f2655ee69bc8fe2b4442965
Adobyzer <adam.abed02@hotmail.com>
2023-10-30 14:24:42 -0400

    add a res.sendFile on the /path to connect the html file

--- myApp.js
+++ myApp.js
@@ -3,24 +3,42 @@ let express = require('express');
 
 //create a express object with field and methods
 let app = express();
-const port = 3000;
 
+
+/*
+const port = 3000;
 app.listen(port, () => {
 console.log("important method server listen on port : ", port);
 })
+*/
+
 //#1 log "Hello World"
 
-console.log("Hello World");
+//console.log("Hello World");
 
-//#2 app.method(path,handler ) ex app.get(/,req,res,next) or app.port(/,req,res,next)
+/*#2 app.method(path,handler )
+
+ ex app.get(/,req,res,next) or app.port(/,req,res,next)
+handler = function(req,res,next)
+req = requet
+res = response
 
 app.get("/",function(req,res){
     res.send("Hello Express");
-    res.send
 });
+*/
 
 
+/*
+#3  res.sendFile(path) method
+*/
 
+let absolutePath = __dirname +'/views/index.html'
+
+app.get('/',function(req,res,next){
+
+res.sendFile(absolutePath);
+});
 
 
 
