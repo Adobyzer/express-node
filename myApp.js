3f771cecafe9ebb14937d60e1fa8ad88f236ef9a
Adobyzer <adam.abed02@hotmail.com>
2025-04-19 02:11:26 -0400

    Refractor all tasks Basic Node and Express module FCC

--- myApp.js
+++ myApp.js
@@ -26,92 +26,13 @@ app.get('/',function(req,res,next){
 res.sendFile(absolutePathHtml);
 });
 
-//#4 middle ware express.static(path) for (static file sucha s css , script , image)
+//#4 middleware express.static(path) for (static file sucha s css , script , image)
 app.use('/public',express.static(__dirname +'/public'));
 
-
-/*
+//#5 use the env file to set the message style
 require('dotenv').config()
 
-
-
-const bodyParser = require('body-parser');
-
-
-//step 11  mount middle ware bodyParser
-
-
-app.use(bodyParser.urlencoded({extended: false}));
-
-//chain a  middleware to a get route 
-
-/*
-In the route app.get('/now', ...) chain a middleware function and the final handler. 
-In the middleware function you should add the current time to the request object in the req.time key. You can use new Date().toString(). 
-In the handler, respond with a JSON object, taking the structure {time: req.time}.
-
-app.get('/now',(req,res) =>{
-req.time = new Date().toString
-next();
-}, (req,res) =>{
-res.json({"time " : req.time});
-});
-//use middleware to logger (step 7)
-app.use((req,res,next)=>{
-    const logMessage = `${req.method} ${req.path} - ${req.ip}`;
-    console.log(logMessage);
-    next(); // Call next to move to the next middleware or route handler REALLY IMPORTANT
-})
-
-//use middleware (step 4)
-app.use('/public',express.static(__dirname + '/public'));
- 
-/*
-const port = 3000;
-app.listen(port, () => {
-console.log("important method server listen on port : ", port);
-})
-*/
-
-//#1 log "Hello World"
-
-//console.log("Hello World");
-
-/*#2 app.method(path,handler )
-
- ex app.get(/,req,res,next) or app.port(/,req,res,next)
- get = fetch info without modiyfing anything
- post = add info (so creating something)
-handler = function(req,res,next)
-req = requet
-res = response
-
-app.get("/",function(req,res){
-    res.send("Hello Express");
-});
-
-
-
-
-
-
-
-
-
-
-
-
-/*#5creating a simple API(serve data) route 
-
-app.get('/json',function(req,res,next){
-
-res.json({"message": "Hello json"});
-});
-
-
-
-
-app.get('/json',function(req,res){
+app.get('/secretjson',function(req,res){
 
     
     let mySecret = process.env.MESSAGE_STYLE;
@@ -127,60 +48,48 @@ app.get('/json',function(req,res){
 
 });
 
+//#6 use middleware to logger 
+app.use((req,res,next)=>{
+    const logMessage = `${req.method} ${req.path} - ${req.ip}`;
+    console.log(logMessage);
+    next(); // Call next to move to the next middleware or route handler REALLY IMPORTANT
+})
+
+//#7 middleware chain to route to create time server
 
 
+app.get('/now',(req,res) =>{
+    req.time = new Date().toString
+    next();
+    }, (req,res) =>{
+    res.json({"time " : req.time});
+    });
 
-app.get('/:word/echo',(req,res) =>{
+//#8 Get Route Parameter Input from the Client
 
-    res.json({"echo": req.params.word})
-  
-  })
+app.get("/:word/echo",(req,res)=>{
+    console.log(req.params)
+})
 
+//#9 Get Query parameter Input from the Client
 
-
-  app.get('/name',(req,res) =>{
+app.get('/name',(req,res) =>{
 
     res.send({"name" : req.query.first + " "+ req.query.last});
     
   })
 
-  
 
- 
+//#10 Use body-parser to Parse POST Requests
 
-  app.post('/name', (req, res) => {
+const bodyparser=require('body-parser')
 
-    res.send("name : " + req.body.first + " " + req.body.last);
-  
-    console.log(req.body.first + " " + req.body.last);
+app.use(bodyparser.urlencoded({extended: false}))
+
+//#11 Get Data from POST Requests
+
+app.post('/name',(req,res) =>{
+
+    res.send({"name" : req.body.first + " "+ req.body.last});
   })
 
-  */
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- //module.exports = app;
